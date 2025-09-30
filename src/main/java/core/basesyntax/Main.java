f749b34f7a9e00e4ff76f4ba99528b25d1e07ddd
Yaroslav Pryshchepa <yrikpa200022@gmail.com>
2025-09-30 21:55:29 +0300

    Implemented Thread pool

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,12 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,7 +15,22 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        try {
+            List<Future<String>> futures = new ArrayList<>();
+            Callable<String> callable = new MyThread();
+            for (int i = 0; i < 20; i++) {
+                Future<String> future = executor.submit(callable);
+                futures.add(future);
+            }
+
+            for (Future<String> future : futures) {
+                logger.log(Level.INFO, future.get());
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            executor.shutdown();
+        }
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,15 @@
 package core.basesyntax;
 
+import java.time.LocalDateTime;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        int random = new Random().nextInt(300);
+        Thread.sleep(random);
+        return String.format("Task duration was %s ms, execution finished at %s",
+                random, LocalDateTime.now());
     }
 }
